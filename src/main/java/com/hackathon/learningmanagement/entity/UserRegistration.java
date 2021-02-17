a9a1e9ae79cc7aff8c72b1c32ea0b65ebe5f91ab
sahanabn90 <78947055+sahanabn90@users.noreply.github.com>
2021-02-17 13:48:48 +0530

    Add files via upload

---
+++ src/main/java/com/hackathon/learningmanagement/entity/UserRegistration.java
@@ -0,0 +1,52 @@
+package com.hackathon.learningmanagement.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_registration")
+public class UserRegistration {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	private Long userName;
+	private String emailId;
+	private String password;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getUserName() {
+		return userName;
+	}
+
+	public void setUserName(Long userName) {
+		this.userName = userName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
